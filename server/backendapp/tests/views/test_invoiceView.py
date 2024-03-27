aee32972a84d3700d905bb3e48ded62996d1f2ab
devonwintz <samuelwintz@gmail.com>
2024-03-27 15:28:11 -0400

    Refactored invoice view tests for single assertion

--- server/backendapp/tests/views/test_invoiceView.py
+++ server/backendapp/tests/views/test_invoiceView.py
@@ -14,12 +14,15 @@ class InvoiceViewTest(TestCase):
     def _get_invoice_detail_url(self, invoice_id):
         return reverse('invoice-detail', kwargs={'id': invoice_id})
 
-    def test_invoice_list_GET(self):
+    def test_invoice_list_GET_status(self):
         response = self.client.get(reverse('invoice-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invoice_list_GET_count(self):
+        response = self.client.get(reverse('invoice-list'))
         self.assertEqual(len(response.json()['data']), len(self.initial_invoices))
 
-    def test_invoice_list_POST_success(self):
+    def test_invoice_list_POST_success_status(self):
         data = {
             'customer': self.customer.id,
             'package': self.package.id,
@@ -27,9 +30,17 @@ class InvoiceViewTest(TestCase):
         }
         response = self.client.post(reverse('invoice-list'), data=data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_invoice_list_POST_success_count(self):
+        data = {
+            'customer': self.customer.id,
+            'package': self.package.id,
+            'price': 20.00
+        }
+        response = self.client.post(reverse('invoice-list'), data=data, content_type='application/json')
         self.assertEqual(Invoice.objects.count(), len(self.initial_invoices) + 1)
 
-    def test_invoice_list_POST_error(self):
+    def test_invoice_list_POST_error_status(self):
         invalid_customer_id = 999
         data = {
             'customer': invalid_customer_id,
@@ -39,18 +50,21 @@ class InvoiceViewTest(TestCase):
         response = self.client.post(reverse('invoice-list'), data=data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_invoice_detail_GET_success(self):
+    def test_invoice_detail_GET_success_status(self):
         response = self.client.get(self._get_invoice_detail_url(self.initial_invoices[0].id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_invoice_detail_GET_error(self):
+    def test_invoice_detail_GET_invalid_invoice_error_status(self):
         invalid_invoice_id = 999
         response = self.client.get(self._get_invoice_detail_url(invalid_invoice_id))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_invoice_detail_GET_invalid_invoice_error_message(self):
+        invalid_invoice_id = 999
+        response = self.client.get(self._get_invoice_detail_url(invalid_invoice_id))
         self.assertEqual(response.json().get('error'), 'Invoice not found')
 
-
-    def test_invoice_detail_PUT_success(self):
+    def test_invoice_detail_PUT_success_status(self):
         updated_price = 40.00
         data = {
             'customer': self.customer.id,
@@ -59,11 +73,19 @@ class InvoiceViewTest(TestCase):
         }
         response = self.client.put(self._get_invoice_detail_url(self.initial_invoices[0].id), data=data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invoice_detail_PUT_success_updated_value(self):
+        updated_price = 40.00
+        data = {
+            'customer': self.customer.id,
+            'package': self.package.id,
+            'price': updated_price
+        }
+        response = self.client.put(self._get_invoice_detail_url(self.initial_invoices[0].id), data=data, content_type='application/json')
         updated_invoice = Invoice.objects.get(pk=self.initial_invoices[0].id)
         self.assertEqual(updated_invoice.price, updated_price)
 
-    def test_invoice_detail_PUT_errors(self):
-        # Test incomplete data (400 Error)
+    def test_invoice_detail_PUT_incomplete_data_error(self):
         incomplete_data = {
             'package': self.package.id,
             'price': 20.00
@@ -71,16 +93,16 @@ class InvoiceViewTest(TestCase):
         response = self.client.put(self._get_invoice_detail_url(self.initial_invoices[0].id), data=incomplete_data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-        # Test invalid invoice ID (404 Error)
+    def test_invoice_detail_PUT_not_found_error(self):
         invalid_invoice_id = 999
         response = self.client.put(self._get_invoice_detail_url(invalid_invoice_id), data={}, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_invoice_detail_DELETE_success(self):
+    def test_invoice_detail_DELETE_success_status(self):
         response = self.client.delete(self._get_invoice_detail_url(self.initial_invoices[0].id))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_invoice_detail_DELETE_error(self):
+    def test_invoice_detail_DELETE_not_found_error(self):
         invalid_invoice_id = 999
         response = self.client.delete(self._get_invoice_detail_url(invalid_invoice_id))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
