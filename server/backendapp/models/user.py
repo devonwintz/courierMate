87d9b68c8ca42eecba63f5cb8ed8afaa9466ff9b
devonwintz <samuelwintz.com>
2024-04-02 21:26:49 -0400

    Updated to make email mandatory

--- server/backendapp/models/user.py
+++ server/backendapp/models/user.py
@@ -26,7 +26,7 @@ class CustomUserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=255, null=False)
     last_name = models.CharField(max_length=255, null=False)
-    email = models.EmailField(max_length=255, unique=True, null=True)
+    email = models.EmailField(max_length=255, unique=True, null=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
