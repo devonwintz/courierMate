38c4e825ba76d3d166c87453fa0078e084358fff
devonwintz <samuelwintz.com>
2024-04-04 15:34:20 -0400

    Removed default price value

--- server/backendapp/models/invoice.py
+++ server/backendapp/models/invoice.py
@@ -7,7 +7,7 @@ class Invoice(models.Model):
     invoice_no = models.CharField(max_length=100, unique=True, null=False, editable=False)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     package = models.ForeignKey(Package, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0.00)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     created = models.DateTimeField(auto_now_add=True, null=False)
     created_by = models.CharField(max_length=255, null=False, default='Admin')
     updated = models.DateTimeField(auto_now=True, null=False)
@@ -21,7 +21,7 @@ class Invoice(models.Model):
     def generate_invoice_no(self):
         return uuid.uuid4().hex[:12].upper()
 
-    def __str__(self): 
+    def __str__(self):
         return self.invoice_no
 
     class Meta:
