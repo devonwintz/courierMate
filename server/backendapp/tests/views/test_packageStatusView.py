a7d220befc023a0ea10017af272f6eef4090f440
devonwintz <samuelwintz.com>
2024-04-11 21:48:58 -0400

    Updated package status view & test cases to include exception handling

--- server/backendapp/tests/views/test_packageStatusView.py
+++ server/backendapp/tests/views/test_packageStatusView.py
@@ -3,6 +3,7 @@ from backendapp.tests.factories import PackageStatusFactory
 from backendapp.models import PackageStatus
 from django.urls import reverse
 from rest_framework import status
+from unittest.mock import patch
 
 class PackageStatusViewTest(TestCase):
     def setUp(self):
@@ -19,6 +20,14 @@ class PackageStatusViewTest(TestCase):
     def test_package_status_list_GET_count(self):
         response = self.client.get(reverse('package-status-list'))
         self.assertEqual(len(response.json()['data']), len(self.initial_package_statuses))
+
+    def test_package_status_list_GET_exception_handling(self):
+        with patch('backendapp.models.PackageStatus.objects.all') as mock_get_package_statuses:
+            mock_get_package_statuses.side_effect = Exception("Test Exception")
+            response = self.client.get(reverse('package-status-list'))
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to retrieve package statuses')
 
     def test_package_status_list_POST_success_status(self):
         data = {
@@ -42,10 +51,21 @@ class PackageStatusViewTest(TestCase):
         new_package_status = PackageStatus.objects.last()
         self.assertEqual(new_package_status.name, 'Available for pickup')
 
-    def test_package_status_list_POST_incomplete_data_error(self):
+    def test_package_status_list_POST_bad_request_error(self):
         incomplete_data = {}
         response = self.client.post(reverse('package-status-list'), data=incomplete_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_package_status_list_POST_exception_handling(self):
+        with patch('backendapp.serializers.PackageStatusSerializer.save') as mock_save:
+            mock_save.side_effect = Exception("Test Exception")
+            data = {
+                    'name': 'Available for pickup'
+                }
+            response = self.client.post(reverse('package-status-list'), data=data, content_type='application/json')
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to create package status')
 
     def test_package_status_detail_GET_success_status(self):
         response = self.client.get(self._get_package_status_detail_url(self.initial_package_statuses[0].id))
@@ -60,6 +80,15 @@ class PackageStatusViewTest(TestCase):
         invalid_package_status = 9999
         response = self.client.get(self._get_package_status_detail_url(invalid_package_status))
         self.assertEqual(response.json().get('error'), 'Package status not found')
+
+    def test_package_statis_detail_GET_exception_handling(self):
+        with patch('backendapp.models.PackageStatus.objects.get') as mock_get_package_status:
+            mock_get_package_status.side_effect = Exception("Test Exception")
+            response = self.client.get(self._get_package_status_detail_url(self.initial_package_statuses[0].id))
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to retrieve package status details')
+
 
     def test_package_status_detail_PUT_success_status(self):
         updated_name = 'Delivered'
@@ -78,7 +107,7 @@ class PackageStatusViewTest(TestCase):
         updated_package_status = PackageStatus.objects.get(pk=self.initial_package_statuses[0].id)
         self.assertEqual(updated_package_status.name, updated_name)
 
-    def test_package_status_detail_PUT_incomplete_data_error(self):
+    def test_package_status_detail_PUT_bad_request_error(self):
         incomplete_data = {}
         response = self.client.put(self._get_package_status_detail_url(self.initial_package_statuses[0].id), data=incomplete_data, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -88,6 +117,18 @@ class PackageStatusViewTest(TestCase):
         response = self.client.put(self._get_package_status_detail_url(invalid_package_status), data={}, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+    def test_package_status_detail_PUT_exception_handling(self):
+        updated_name = 'Delivered'
+        data = {
+            'name': updated_name
+        }
+        with patch('backendapp.serializers.PackageStatusSerializer.save') as mock_save:
+            mock_save.side_effect = Exception("Test Exception")
+            response = self.client.put(self._get_package_status_detail_url(self.initial_package_statuses[0].id), data=data, content_type='application/json')
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to update package status details')
+
     def test_package_status_detail_DELETE_success_status(self):
         response = self.client.delete(self._get_package_status_detail_url(self.initial_package_statuses[0].id))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
@@ -95,3 +136,11 @@ class PackageStatusViewTest(TestCase):
     def test_package_status_detail_DELETE_not_found_error(self):
         response = self.client.delete(self._get_package_status_detail_url(9999))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_package_status_detail_DELETE_exception_handling(self):
+        with patch('backendapp.models.PackageStatus.delete') as mock_delete:
+            mock_delete.side_effect = Exception("Test Exception")
+            response = self.client.delete(self._get_package_status_detail_url(self.initial_package_statuses[0].id))
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to delete package status')
