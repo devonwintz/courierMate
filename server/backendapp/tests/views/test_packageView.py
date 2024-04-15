066d28215fa7d2eb91a8c1510783a4994f3771dd
devonwintz <samuelwintz.com>
2024-04-15 10:30:10 -0400

    Updated package view & test cases to include exception handling

--- server/backendapp/tests/views/test_packageView.py
+++ server/backendapp/tests/views/test_packageView.py
@@ -3,6 +3,7 @@ from backendapp.tests.factories import PackageFactory, CustomerFactory, PackageC
 from backendapp.models import Package
 from django.urls import reverse
 from rest_framework import status
+from unittest.mock import patch
 
 class PackageViewTest(TestCase):
     def setUp(self):
@@ -22,6 +23,14 @@ class PackageViewTest(TestCase):
     def test_package_list_GET_count(self):
         response = self.client.get(reverse('package-list'))
         self.assertEqual(len(response.json()['data']), len(self.initial_packages))
+
+    def test_package_list_GET_exception_handling(self):
+        with patch('backendapp.models.Package.objects.all') as mock_get_packages:
+            mock_get_packages.side_effect = Exception("Test Exception")
+            response = self.client.get(reverse('package-list'))
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to retrieve packages')
 
     def test_package_list_POST_success_status(self):
         data = {
@@ -57,7 +66,7 @@ class PackageViewTest(TestCase):
         new_package = Package.objects.last()
         self.assertEqual(new_package.tracking_number, '12345678910')
 
-    def test_package_list_POST_invalid_customer_error(self):
+    def test_package_list_POST_bad_request_error(self):
         invalid_customer_id = 9999
         data = {
             'customer': invalid_customer_id,
@@ -67,6 +76,21 @@ class PackageViewTest(TestCase):
         }
         response = self.client.post(reverse('package-list'), data=data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_package_list_POST_exception_handling(self):
+        with patch('backendapp.serializers.CreatePackageSerializer.save') as mock_save:
+            mock_save.side_effect = Exception("Test Exception")
+            data = {
+                    'customer': self.customer.id,
+                    'tracking_number': '12345678910',
+                    'date_delivered_ny': '2024-04-01',
+                    'category': self.package_category.id,
+                    'status': self.package_status.id
+                }
+            response = self.client.post(reverse('package-list'), data=data, content_type='application/json')
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to create package')
 
     def test_package_detail_GET_success_status(self):
         response = self.client.get(self._get_package_detail_url(self.initial_packages[0].id))
@@ -79,6 +103,14 @@ class PackageViewTest(TestCase):
     def test_package_detail_GET_error_message(self):
         response = self.client.get(self._get_package_detail_url(9999))
         self.assertEqual(response.json().get('error'), 'Package not found')
+
+    def test_package_detail_GET_exception_handling(self):
+        with patch('backendapp.models.Package.objects.get') as mock_get_package:
+            mock_get_package.side_effect = Exception("Test Exception")
+            response = self.client.get(self._get_package_detail_url(self.initial_packages[0].id))
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to retrieve package details')
 
     def test_package_detail_PUT_success_status(self):
         updated_tracking_number = '0123456789'
@@ -103,10 +135,36 @@ class PackageViewTest(TestCase):
         updated_package = Package.objects.get(pk=self.initial_packages[0].id)
         self.assertEqual(updated_package.tracking_number, updated_tracking_number)
 
+    def test_package_detail_PUT_bad_request_error(self):
+        updated_tracking_number = None
+        data = {
+            'customer': self.customer.id,
+            'tracking_number': updated_tracking_number,
+            'category': self.package_category.id,
+            'status': self.package_status.id
+        }
+        response = self.client.put(self._get_package_detail_url(self.initial_packages[0].id), data=data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_package_detail_PUT_not_found_errors(self):
         invalid_package_id = 9999
         response = self.client.put(self._get_package_detail_url(invalid_package_id), data={}, content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_package_detail_PUT_exception_handling(self):
+        updated_tracking_number = '0123456789'
+        data = {
+            'customer': self.customer.id,
+            'tracking_number': updated_tracking_number,
+            'category': self.package_category.id,
+            'status': self.package_status.id
+        }
+        with patch('backendapp.serializers.UpdatePackageSerializer.save') as mock_save:
+            mock_save.side_effect = Exception("Test Exception")
+            response = self.client.put(self._get_package_detail_url(self.initial_packages[0].id), data=data, content_type='application/json')
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to update package details')
 
     def test_package_detail_DELETE_success_status(self):
         response = self.client.delete(self._get_package_detail_url(self.initial_packages[0].id))
@@ -115,3 +173,11 @@ class PackageViewTest(TestCase):
     def test_package_detail_DELETE_not_found_error(self):
         response = self.client.delete(self._get_package_detail_url(9999))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_package_detail_DELETE_exception_handling(self):
+        with patch('backendapp.models.Package.delete') as mock_delete:
+            mock_delete.side_effect = Exception("Test Exception")
+            response = self.client.delete(self._get_package_detail_url(self.initial_packages[0].id))
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.data['status'], 'error')
+            self.assertEqual(response.data['error'], 'Failed to delete package')
--- server/backendapp/views/package.py
+++ server/backendapp/views/package.py
@@ -7,86 +7,115 @@ from rest_framework.decorators import api_view
 
 class PackageList(APIView):
     def get(self, request):
-        packages = Package.objects.all()
-        serializer = CreatePackageSerializer(packages, many=True)
-        return Response({
-            'status': 'success',
-            'data': serializer.data,
-            'error': None,
-        }, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = CreatePackageSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+        try:
+            packages = Package.objects.all()
+            serializer = CreatePackageSerializer(packages, many=True)
             return Response({
                 'status': 'success',
                 'data': serializer.data,
-                'error': None
-            }, status=status.HTTP_201_CREATED)
-        else:
-            return Response({
+                'error': None,
+            }, status=status.HTTP_200_OK)
+        except Exception as e:
+             return Response({
                 'status': 'error',
                 'data': None,
-                'error': serializer.errors
-            }, status=status.HTTP_400_BAD_REQUEST)
+                'error': 'Failed to retrieve packages'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def post(self, request):
+        try:
+            serializer = CreatePackageSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status': 'success',
+                    'data': serializer.data,
+                    'error': None
+                }, status=status.HTTP_201_CREATED)
+            else:
+                return Response({
+                    'status': 'error',
+                    'data': None,
+                    'error': serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+             return Response({
+                'status': 'error',
+                'data': None,
+                'error': 'Failed to create package'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class PackageDetail(APIView):
     def get(self, request, id):
         try:
             package = Package.objects.get(pk=id)
-        except Package.DoesNotExist:
-            return Response({
-                'status': 'error',
-                'data': None,
-                'error': 'Package not found'
-            }, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = CreatePackageSerializer(package)
-        return Response({
-            'status': 'success',
-            'data': serializer.data,
-            'error': None
-        }, status=status.HTTP_200_OK)
-
-    def put(self, request, id):
-        try:
-            package = Package.objects.get(pk=id)
-        except Package.DoesNotExist:
-            return Response({
-                'status': 'error',
-                'data': None,
-                'error': 'Package not found'
-            }, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = UpdatePackageSerializer(package, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+            serializer = CreatePackageSerializer(package)
             return Response({
                 'status': 'success',
                 'data': serializer.data,
                 'error': None
             }, status=status.HTTP_200_OK)
-        else:
-            return Response({
-                'status': 'error',
-                'data': None,
-                'error': serializer.errors
-            }, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        try:
-            package = Package.objects.get(pk=id)
         except Package.DoesNotExist:
             return Response({
                 'status': 'error',
                 'data': None,
                 'error': 'Package not found'
             }, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+             return Response({
+                'status': 'error',
+                'data': None,
+                'error': 'Failed to retrieve package details'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        package.delete()
-        return Response({
-            'status': 'success',
-            'data': None,
-            'error': None
-        }, status=status.HTTP_204_NO_CONTENT)
+    def put(self, request, id):
+        try:
+            package = Package.objects.get(pk=id)
+            serializer = UpdatePackageSerializer(package, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status': 'success',
+                    'data': serializer.data,
+                    'error': None
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    'status': 'error',
+                    'data': None,
+                    'error': serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except Package.DoesNotExist:
+            return Response({
+                'status': 'error',
+                'data': None,
+                'error': 'Package not found'
+            }, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+             return Response({
+                'status': 'error',
+                'data': None,
+                'error': 'Failed to update package details'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, request, id):
+        try:
+            package = Package.objects.get(pk=id)
+            package.delete()
+            return Response({
+                'status': 'success',
+                'data': None,
+                'error': None
+            }, status=status.HTTP_204_NO_CONTENT)
+        except Package.DoesNotExist:
+            return Response({
+                'status': 'error',
+                'data': None,
+                'error': 'Package not found'
+            }, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+             return Response({
+                'status': 'error',
+                'data': None,
+                'error': 'Failed to delete package'
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
